3bd8cff223b96e12d1d91b8290fdab7d8ed979d8
hmdhszd <hmdhszd@gmail.com>
2021-10-07 12:22:37 +0330

    modified:   main.go

--- String-Interpolation-examples-in-Golang/go.mod
+++ String-Interpolation-examples-in-Golang/go.mod
@@ -1,3 +1,8 @@
 module myapp
 
 go 1.17
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807 // indirect
+	golang.org/x/sys v0.0.0-20211006225509-1a26e0398eed // indirect
+)
--- String-Interpolation-examples-in-Golang/main.go
+++ String-Interpolation-examples-in-Golang/main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
+
+type User struct {
+	UserName       string
+	Age            int64
+	FavoriteNumber float64
+	OwnsAdog       bool
+}
 
 var reader *bufio.Reader
 
 func main() {
-	reader = bufio.NewReader(os.Stdin)
-	userName := readString("What is your name?")
-	age := readInt("How old are you?")
+	var user User
 
-	fmt.Println("your name is ", userName, ", and you are", age, "years old")
-	fmt.Printf("Your name is %s and you are %d years old.", userName, age)
+	reader = bufio.NewReader(os.Stdin)
+
+	user.UserName = readString("What is your name?")
+	user.Age = readInt("How old are you?")
+	user.FavoriteNumber = readFloat("What is your favorite number?")
+	user.OwnsAdog = readBool("Do you own a dog? (y/n)")
+
+	fmt.Printf("Your name is %s and you are %d years old. Your favorite number is %f . Own a dog: %t", user.UserName, user.Age, user.FavoriteNumber, user.OwnsAdog)
 
 }
 
@@ -42,7 +56,7 @@ func readString(s string) string {
 	}
 }
 
-func readInt(s string) int {
+func readInt(s string) int64 {
 
 	for {
 		fmt.Println(s)
@@ -54,11 +68,65 @@ func readInt(s string) int {
 		num, err := strconv.Atoi(userInput)
 
 		if err != nil {
-			fmt.Println("Please enter only numbers !")
+			fmt.Println("Please enter a whole numbers !")
+
+		} else {
+			return int64(num)
+		}
+
+	}
+}
+
+func readFloat(s string) float64 {
+
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput, 64)
+
+		if err != nil {
+			fmt.Println("Please enter a numbers !")
 
 		} else {
 			return num
 		}
 
 	}
+}
+
+func readBool(s string) bool {
+	err := keyboard.Open()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+		fmt.Println(s)
+
+		char, _, err := keyboard.GetSingleKey()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.ToLower(string(char)) != "y" && strings.ToLower(string(char)) != "n" {
+			fmt.Println("Please Enter y or n")
+
+		} else if char == 'n' || char == 'N' {
+			return false
+
+		} else if char == 'y' || char == 'Y' {
+			return true
+		}
+	}
+
 }
