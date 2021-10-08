19b204d87c6ea8ec21500df8f66110ffedebd1dc
hmdhszd <hmdhszd@gmail.com>
2021-10-08 23:43:45 +0330

    new file:   Data Types in Go (interfaces)/main.go

---
+++ Data Types in Go (interfaces)/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Says() string
+	HowManyLegs() int
+}
+
+type Dog struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+func (d *Dog) Says() string {
+	return d.Sound
+}
+
+func (d *Dog) HowManyLegs() int {
+	return d.NumberOfLegs
+}
+
+type Cat struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+	HasTais      bool
+}
+
+func (d *Cat) Says() string {
+	return d.Sound
+}
+
+func (d *Cat) HowManyLegs() int {
+	return d.NumberOfLegs
+}
+
+func main() {
+
+	dog := Dog{
+		Name:         "Dog",
+		Sound:        "Happp happpppp",
+		NumberOfLegs: 4,
+	}
+
+	Riddle(&dog)
+
+	var cat Cat
+	cat.Name = "cat"
+	cat.Sound = "mieuuuu"
+	cat.NumberOfLegs = 4
+	cat.HasTais = true
+
+	Riddle(&cat)
+
+}
+
+func Riddle(a Animal) {
+	riddle := fmt.Sprintf(`This animal says "%s" and has "%d" legs. What animal is it?`, a.Says(), a.HowManyLegs())
+	fmt.Println(riddle)
+}
