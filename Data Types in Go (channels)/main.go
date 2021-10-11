781c9e4fda104b88ef5045618d48acc00559651b
hmdhszd <hmdhszd@gmail.com>
2021-10-11 16:31:22 +0330

    updated Data Types in Go (channels)/main.go

--- Data Types in Go (channels)/main.go
+++ Data Types in Go (channels)/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 
 	go doSomething("Hello, world!")
+	go doAnOtherThing("it does another thing!")
 
 	fmt.Println("this is another message!")
 
@@ -15,6 +16,23 @@ func main() {
 }
 
 func doSomething(s string) {
+
+	until := 0
+
+	for {
+
+		fmt.Println(s)
+		until += 1
+
+		if until >= 5 {
+			break
+		}
+
+	}
+
+}
+
+func doAnOtherThing(s string) {
 
 	until := 0
 
