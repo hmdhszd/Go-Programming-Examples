633ec10babdf291cd831a1190dfa3e18b7218770
hmdhszd <hmdhszd@gmail.com>
2021-10-14 11:08:44 +0330

    modified:   Three Part For Loop Example by Go/main.go

--- Three Part For Loop Example by Go/main.go
+++ Three Part For Loop Example by Go/main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
+
 	for i := 0; i <= 10; i++ {
 		println("i is ->", i)
 	}
+
+	//===============================================================
+
+	rand.Seed(time.Now().UnixNano())
+	i := 1000
+
+	for i > 100 {
+		i = rand.Intn(1000) + 1
+
+		fmt.Println("i is", i)
+
+		if i > 100 {
+			println("i is ", i, "So loop keeps going")
+		}
+	}
+
+	fmt.Println("got", i, "Loop stopped")
+
 }
