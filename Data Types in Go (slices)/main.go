ea11c76e1fd431887b4be0653be03801c9b3325f
hmdhszd <hmdhszd@gmail.com>
2021-12-22 11:52:19 +0330

    modified:   Data Types in Go (slices)/main.go

--- Data Types in Go (slices)/main.go
+++ Data Types in Go (slices)/main.go
@@ -26,4 +26,27 @@ func main() {
 	sort.Strings(animals)
 	fmt.Println("is this slice sorted now?", sort.StringsAreSorted(animals))
 
+	// ------------------------------------------------------------------
+
+	// MyNewSlice := make(type, length, capacity)
+	MyNewSlice := make([]int, 10, 100)
+	fmt.Println(MyNewSlice)
+	fmt.Println(len(MyNewSlice))
+	fmt.Println(cap(MyNewSlice))
+
+	MyNewSlice = append(MyNewSlice, 1234, 21345, 2346, 2345)
+	fmt.Println(MyNewSlice)
+	fmt.Println(len(MyNewSlice))
+	fmt.Println(cap(MyNewSlice))
+
+	// ------------------------------------------------------------------
+	// Multi dimentional slice
+
+	s1 := []string{"Hamid", "Hosseinzadeh", "1991", "Jan", "24"}
+	s2 := []string{"James", "Bond", "1982", "Mai", "02"}
+
+	s12 := [][]string{s1, s2}
+
+	fmt.Println(s12)
+
 }
