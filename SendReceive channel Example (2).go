822548d7248b3118efd2182941243c835add15b3
hmdhszd <hmdhszd@gmail.com>
2021-12-27 13:21:32 +0330

    new file:   SendReceive channel Example (2).go

---
+++ SendReceive channel Example (2).go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	c := make(chan int)
+
+	//send
+	go foo(c)
+
+	//receive
+	for v := range c {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit...")
+}
+
+//send
+func foo(c chan<- int) {
+
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+
+}
